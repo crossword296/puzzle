1750aa10f94f742c653b12729cc19222e7060452
crossword296 <147116559+crossword296@users.noreply.github.com>
2026-05-20 01:55:27 +0900

    Add files via upload

---
+++ randomj.js
@@ -0,0 +1,8 @@
+var txt = new Array();
+txt[0]="parea_3x3.html";
+txt[1]="setaken_3x3.html";
+txt[2]="volubin5_3x3.html";
+
+mmax = 3; //メッセージ行数
+txtno = Math.floor(Math.random() * mmax);
+location.href=txt[txtno];
