85f818ed0a8979a24c6759e663202ab64c3dae9d
crossword296 <147116559+crossword296@users.noreply.github.com>
2026-05-23 15:47:55 +0900

    Add files via upload

--- randomj.js
+++ randomj.js
@@ -2,7 +2,8 @@ var txt = new Array();
 txt[0]="parea_3x3.html";
 txt[1]="setaken_3x3.html";
 txt[2]="volubin5_3x3.html";
+txt[3]="bnbk_3x3.html";
 
-mmax = 3; //メッセージ行数
+mmax = 4; //メッセージ行数
 txtno = Math.floor(Math.random() * mmax);
 location.href=txt[txtno];
